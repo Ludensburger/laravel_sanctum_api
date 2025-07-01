8aa06534d2a5ede380c2fc9de8cd25488ad71920
Ludensburger <mendozaryu08@gmail.com>
2025-07-01 16:44:58 +0800

    Redirect root URL to /home instead of displaying welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+
+// Automatically redirect to /home when accessing the root URL: http://127.0.0.1:8000
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
 
+// or Manually add /home to http://127.0.0.1:8000 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
